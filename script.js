35a27b6eaf715dc5e8d7dff86d706cdf9f7a35c8
Etativel <farhanmaulana4611@gmail.com>
2024-02-12 22:48:42 +0700

    add function to push book into the library list

--- script.js
+++ script.js
@@ -5,9 +5,28 @@ const submitForm = document.querySelector("form")
 
 const bookList = []
 
-function Book(title, author, pages) {
+function Book(title, author, genre, alreadyRead, rating) {
     this.title = title
+    this.author = author
+    this.genre = genre
+    this.alreadyRead = alreadyRead
+    this.rating = rating
 }
+
+function addBookToLibrary(){
+    const title = document.getElementById("book-title-form").value;
+    const author = document.getElementById("book-author-form").value;
+    const genre = document.getElementById("book-genre-form").value;
+    const alreadyRead = document.getElementById("book-read-form").checked;
+    const rating = document.getElementById("book-rating-form").value;
+
+    const currentBook = new Book(title, author, genre, alreadyRead, rating)
+
+    bookList.push(currentBook)
+
+}
+
+
 
 addBookBtn.addEventListener("click", ()=>{
     formDialog.showModal();
@@ -20,15 +39,11 @@ cancelForm.addEventListener("click", (e)=>{
 })
 
 submitForm.addEventListener("submit", (e) => {
-    // e.preventDefault()
-    const title = document.getElementById("book-title-form").value;
-    const author = document.getElementById("book-author-form").value;
-    const genre = document.getElementById("book-genre-form").value;
-    const alreadyRead = document.getElementById("book-read-form").checked;
-    const rating = document.getElementById("book-rating-form").value;
+    addBookToLibrary()
 
-    bookList.push({title,author,genre,alreadyRead,rating})
+    e.preventDefault()
+    submitForm.reset()
+    formDialog.close()
     console.log(bookList)
 
-    // Here you can perform further actions with the form data, like sending it to a server or updating the UI
 });
