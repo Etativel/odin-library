92bcfa6103dcd071ad5130504091491a5fb952b5
Etativel <farhanmaulana4611@gmail.com>
2024-02-12 22:37:53 +0700

    add basic form and javascript for book title, author, etc.

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 const addBookBtn = document.querySelector(".addBook-btn")
+const formDialog = document.querySelector(".formDialog");
+const cancelForm = document.querySelector(".cancel-form")
+const submitForm = document.querySelector("form")
 
+const bookList = []
+
+function Book(title, author, pages) {
+    this.title = title
+}
 
 addBookBtn.addEventListener("click", ()=>{
-    console.log("Hello")
+    formDialog.showModal();
 })
+
+
+cancelForm.addEventListener("click", (e)=>{
+    e.preventDefault()
+    formDialog.close()
+})
+
+submitForm.addEventListener("submit", (e) => {
+    // e.preventDefault()
+    const title = document.getElementById("book-title-form").value;
+    const author = document.getElementById("book-author-form").value;
+    const genre = document.getElementById("book-genre-form").value;
+    const alreadyRead = document.getElementById("book-read-form").checked;
+    const rating = document.getElementById("book-rating-form").value;
+
+    bookList.push({title,author,genre,alreadyRead,rating})
+    console.log(bookList)
+
+    // Here you can perform further actions with the form data, like sending it to a server or updating the UI
+});
